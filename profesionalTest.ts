cc962caee616c23b4aba5e056e7e1a6dfd45537a
CRBox <dharianfenix@gmail.com>
2024-03-02 17:43:30 +0100

    Subida clase Profesional

---
+++ profesionalTest.ts
@@ -0,0 +1,5 @@
+import { Profesional } from "./profesional";
+
+
+let profesional1 = new Profesional('Leonardo DiCaprio', 49, 85, 183, false, 'EEUU', 1, 'actor')
+console.log(profesional1.printAll());
